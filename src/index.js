cb763d3c8cf742acd535581e1932bb19bbdbf363
Zhang <my@qq.com>
2019-03-04 12:02:13 +0800

    新建了一个develop 分支，并初始化

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1> develop分支 </h1>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
